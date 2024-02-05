3fef31d21384215f7cb846c2c157079953de1fea
Dushyant Bhardwaj <dushyantbha012@gmail.com>
2024-02-05 23:52:32 +0530

    added react-router

--- Frontend-Realm/src/main.jsx
+++ Frontend-Realm/src/main.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {recoilRoot} from "recoil"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Layout from "./components/Layout/Layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import { RecoilRoot } from "recoil";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <recoilRoot>
-  <App />
-  </recoilRoot>
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </RouterProvider>
+);
