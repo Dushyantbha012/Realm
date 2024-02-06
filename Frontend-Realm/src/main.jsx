346ccead5b9bf6536995333ed1e71a21b2914a9e
Dushyant Bhardwaj <dushyantbha012@gmail.com>
2024-02-06 21:07:10 +0530

    added different routes to react-router

--- Frontend-Realm/src/main.jsx
+++ Frontend-Realm/src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import Layout from "./components/Layout/Layout.jsx";
 import Home from "./components/Home/Home.jsx";
+import Signup from "./components/Signup/Signup.jsx"
+import Signin from "./components/Signin/Signin.jsx"
+import Chat from "./components/Chat/Chat.jsx"
 import { RecoilRoot } from "recoil";
 import {
   createBrowserRouter,
@@ -16,14 +18,17 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />} />
+      <Route path="signup" element={<Signup />} />
+      <Route path="signin" element={<Signin />} />
+      <Route path="chat" element={<Chat />} />
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router}>
-    <RecoilRoot>
-      <App />
+  
+  <RecoilRoot>
+    <RouterProvider router={router} />
     </RecoilRoot>
-  </RouterProvider>
+    
 );
