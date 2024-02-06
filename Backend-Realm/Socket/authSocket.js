577c52e2f6762a47cad145d63a85e70910e09193
Dushyant Bhardwaj <dushyantbha012@gmail.com>
2024-02-06 16:40:13 +0530

    created authSocket

--- Backend-Realm/Socket/authSocket.js
+++ Backend-Realm/Socket/authSocket.js
@@ -1,5 +1,19 @@
+const jwt = require("jsonwebtoken")
+import {SECRET_KEY} from('../config')
+
 const authSocket = (socket,next)=>{
     const {token}= socket.handshake.query;
+    if(!authHeader){
+        console.log("auth failed 1")
+        return 
+    }
+    try {
+        const decoded = jwt.verify(authHeader, SECRET_KEY);
+        socket.request.userID = decoded.userID;
+        next()
+    } catch (err) {
+        console.log("auth failed 2")
+    }
 }
 
 module.exports = authSocket;
--- Backend-Realm/Socket/joinRoomMiddleWare.js
+++ Backend-Realm/Socket/joinRoomMiddleWare.js
@@ -1,5 +1,14 @@
-const joinRoomMiddleWare = async(socket,next)=>{
+import Room from "../DataBase/db"
 
+const joinRoomMiddleWare = async(socket,roomJoinData)=>{
+    try{
+        const room = await Room.findOne({roomID:roomJoinData.roomID})
+        if(!room) return false;
+        return room.users.includes(socket.request.roomID);
+    }
+    catch{
+        return false;
+    }
 }
 
 module.exports=joinRoomMiddleWare
