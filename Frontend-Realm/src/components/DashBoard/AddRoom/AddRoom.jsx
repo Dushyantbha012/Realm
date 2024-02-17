5554f81968690c13d8b2b0813e45b5d1be13f54f
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2024-02-17 16:02:25 +0530

    Update AddRoom.jsx

--- Frontend-Realm/src/components/DashBoard/AddRoom/AddRoom.jsx
+++ Frontend-Realm/src/components/DashBoard/AddRoom/AddRoom.jsx
@@ -4,18 +4,18 @@ import axios from "axios";
 function AddRoom() {
   const [roomId, setRoomId] = useState("");
   const addRoom = async () => {
-    if(roomId!=""){
-        const res = await axios({
-            method: "POST",
-            url: "http://localhost:3000/api/chat/addroom",
-            headers : {authorization: localStorage.getItem("token") },
-            data : {roomId:roomId}
-          });
-          alert(res.data.message)
-          
+    if (roomId != "") {
+      const res = await axios({
+        method: "POST",
+        url: "http://localhost:3000/api/chat/addroom",
+        headers: { authorization: localStorage.getItem("token") },
+        data: { roomId: roomId }
+      });
+      alert(res.data.message)
+
     }
-    else{
-        alert("Give a name to the Room")
+    else {
+      alert("Give a name to the Room")
     }
   };
   return (
@@ -25,9 +25,18 @@ function AddRoom() {
         value={roomId}
         onChange={(e) => setRoomId(e.target.value)}
       ></input>
-      <button onClick={addRoom}>Add Room</button>
+      <button onClick={addRoom} style={addRoomBtnStyle}>Add Room</button>
     </div>
   );
 }
+
+const addRoomBtnStyle = {
+  padding: '10px 20px',
+  backgroundColor: '#007bff',
+  color: 'white',
+  border: 'none',
+  borderRadius: '5px',
+  cursor: 'pointer',
+};
 
 export default AddRoom;
