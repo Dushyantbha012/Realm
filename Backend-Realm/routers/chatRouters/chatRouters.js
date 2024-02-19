567c20ed86a5d1e326684570999f8da6b832e8da
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2024-02-19 14:28:53 +0530

    Update chatRouters.js

--- Backend-Realm/routers/chatRouters/chatRouters.js
+++ Backend-Realm/routers/chatRouters/chatRouters.js
@@ -8,15 +8,30 @@ const { authMiddleware } = require("../authMiddleware/authMiddleware");
 
 chatRouters.get("/userrooms", authMiddleware, async (req, res) => {
   console.log("request reached in userrooms")
+  const filter = req.query.filter || "";
+  console.log("filter is ", filter)
   const user = await User.findOne({ _id: req.userId });
   const chatRooms = user.rooms;
-  const room=chatRooms.map((room) => ({
-    roomId: room,
-  }))
-  console.log("user rooms are ",room)
+
+  if (filter != "") {
+    const filteredRooms = chatRooms.filter(room => room.includes(filter));
+
+    const room = filteredRooms.map(room => ({
+      roomId: room,
+    }));
+    console.log("user rooms are ", room)
   res.json({
-    room:room
+    room: room
   });
+  }
+  else {
+    const room = chatRooms.map(room => ({roomId:room}))
+    console.log("user rooms are ", room)
+  res.json({
+    room: room
+  });
+  }
+  
 });
 
 chatRouters.get("/allrooms", authMiddleware, async (req, res) => {
@@ -27,55 +42,56 @@ chatRouters.get("/allrooms", authMiddleware, async (req, res) => {
       $regex: filter,
     },
   });
-  const room= rooms.map((room) => ({
+  const room = rooms.map((room) => ({
     roomId: room.roomId,
     roomdbId: room._id,
   }))
   console.log("sent is ", room)
-  res.json({room:room
+  res.json({
+    room: room
   });
 });
 
 chatRouters.post("/addroom", authMiddleware, async (req, res) => {
- try{
-  const roomId = req.body.roomId;
-  const existingRoom = await Room.findOne({
-    roomId: roomId,
-  });
-  if (existingRoom) {
-    return res.status(411).json({ message: "Room already exists" });
+  try {
+    const roomId = req.body.roomId;
+    const existingRoom = await Room.findOne({
+      roomId: roomId,
+    });
+    if (existingRoom) {
+      return res.status(411).json({ message: "Room already exists" });
+    }
+    const room = {
+      roomId: roomId,
+      chats: [],
+      users: [req.userId],
+    };
+    const dbRoom = await Room.create(room);
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: req.userId, },
+      { $addToSet: { rooms: room.roomId } },
+      { new: true }
+    );
+    console.log("updated user room is ", updatedUser.rooms);
+    res.json({ message: "Room created" });
   }
-  const room = {
-    roomId: roomId,
-    chats: [],
-    users: [req.userId],
-  };
-  const dbRoom=await Room.create(room);
-  const updatedUser = await User.findOneAndUpdate(
-    { _id: req.userId,},
-    { $addToSet: { rooms: room.roomId }  },
-    { new: true }
-  );
-  console.log("updated user room is ", updatedUser.rooms);
-  res.json({ message: "Room created" });
- }
- catch{
-  res.status(411).json({message:"not able to create room"})
- }
+  catch {
+    res.status(411).json({ message: "not able to create room" })
+  }
 });
 
 chatRouters.post("/joinchat", authMiddleware, async (req, res) => {
   const updatedRoom = await Room.findOneAndUpdate(
-    { _id: req.body.roomdbId,},
+    { _id: req.body.roomdbId, },
     { $addToSet: { users: req.userId } },
     { new: true }
   );
   const updatedUser = await User.findOneAndUpdate(
-    { _id: req.userId,},
-    { $addToSet: { rooms: updatedRoom.roomId} },
+    { _id: req.userId, },
+    { $addToSet: { rooms: updatedRoom.roomId } },
     { new: true }
   );
-  
+
   res.json({ message: "added to the room" });
 });
 
