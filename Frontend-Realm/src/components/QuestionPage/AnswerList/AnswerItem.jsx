9f91e624812f6690dd153935480fe2328babd0a3
Dushyant Bhardwaj <dushyantbha012@gmail.com>
2024-02-25 04:35:27 +0530

    final design changes

--- Frontend-Realm/src/components/QuestionPage/AnswerList/AnswerItem.jsx
+++ Frontend-Realm/src/components/QuestionPage/AnswerList/AnswerItem.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import axios from "axios"
-import './AnswerList.css'
+import axios from "axios";
+import "./AnswerList.css";
 function AnswerItem({
   content = "",
   author = "",
   likes = 0,
   dislikes = 0,
   ansdbId = "",
-}){
+}) {
   const likeAnswer = async () => {
     const res = await axios({
       method: "POST",
       url: "http://localhost:3000/api/question/likeanswer",
-      data: {ansId:ansdbId},
+      data: { ansId: ansdbId },
       headers: { Authorization: localStorage.getItem("token") },
     });
-    if(res.status===200){
+    if (res.status === 200) {
       window.location.reload(false);
     }
   };
@@ -23,17 +23,23 @@ function AnswerItem({
     const res = await axios({
       method: "POST",
       url: "http://localhost:3000/api/question/dislikeanswer",
-      data: {ansId:ansdbId},
+      data: { ansId: ansdbId },
       headers: { Authorization: localStorage.getItem("token") },
     });
-    if(res.status===200){
+    if (res.status === 200) {
       window.location.reload(false);
     }
   };
   return (
-    <div>
-      <div>{content}</div>
-      <div className="author">Answered By : {author}</div>
+    <div className="flex  flex-row flex-wrap align-top rounded-lg bg-green-200 p-2 my-2">
+      <div className="w-full">{content}</div>
+      <div className="flex flex-row flex-nowrap align-middle items-center ">
+        <div className="author mx-2">{author}</div>
+        <div onClick={likeAnswer} className="mx-2 min-w-[100px] bg-blue-500 rounded-lg text-white text-center">
+          Likes  {likes}
+        </div>
+        <div onClick={dislikeAnswer} className="mx-2 min-w-[100px]  bg-blue-500 rounded-lg text-white text-center">Dislikes  {dislikes}</div>
+      </div>
     </div>
   );
 }
--- Frontend-Realm/src/components/QuestionPage/AnswerList/AnswerList.jsx
+++ Frontend-Realm/src/components/QuestionPage/AnswerList/AnswerList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import AnswerItem from "./AnswerItem";
-import './AnswerList.css'
+import "./AnswerList.css";
 
 function AnswerList() {
   const [data, setData] = useState({
@@ -76,41 +76,41 @@ function AnswerList() {
 
   return (
     <div className="answer-page">
-    <div className="ans-container">
-      <div className="ans-header">
-        <h2 className="title">Question : {data.title}</h2>
-        <p className="author">Author: {data.author}</p>
-        <div className="like-dislike">
-          <button onClick={likeQues}>Like ({data.likes})</button>
-          <button onClick={dislikeQues}>Dislike ({data.dislikes})</button>
+      <div className="ans-container">
+        <div className="ans-header">
+          <h2 className="title">Question : {data.title}</h2>
+          <p className="author">Author: {data.author}</p>
+          <div className="like-dislike">
+            <button onClick={likeQues}>Like ({data.likes})</button>
+            <button onClick={dislikeQues}>Dislike ({data.dislikes})</button>
+          </div>
+        </div>
+
+        <div className="answer-list">
+          {data.answers.map((answer) => (
+            <div>
+              <AnswerItem
+                key={answer.ansdbId}
+                content={answer.content}
+                author={answer.author}
+                likes={answer.likes}
+                dislikes={answer.dislikes}
+                ansdbId={answer.ansdbId}
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="submit-answer">
+          <input
+            onChange={(e) => setInputAnswer(e.target.value)}
+            type="text"
+            value={inputAnswer}
+            placeholder="Answer..."
+          />
+          <button onClick={submitAnswer}>Submit Answer</button>
         </div>
       </div>
-      
-      <div className="answer-list">
-        {data.answers.map((answer) => (
-          <div>
-          <AnswerItem
-            key={answer.ansdbId} 
-            content={answer.content}
-            author={answer.author}
-            likes={answer.likes}
-            dislikes={answer.dislikes}
-            ansdbId={answer.ansdbId}
-          />
-          </div>
-        ))}
-      </div>
-      
-      <div className="submit-answer">
-        <input
-          onChange={(e) => setInputAnswer(e.target.value)}
-          type="text"
-          value={inputAnswer}
-          placeholder="Answer..."
-        />
-        <button onClick={submitAnswer}>Submit Answer</button>
-      </div>
-    </div>
     </div>
   );
 }
