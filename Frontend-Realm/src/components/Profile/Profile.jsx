e47171a8c888658ab9469bf77d0b666f498efdeb
Dushyant Bhardwaj <dushyantbha012@gmail.com>
2024-02-24 11:44:10 +0530

    added styling to profile

--- Frontend-Realm/src/components/Profile/Profile.jsx
+++ Frontend-Realm/src/components/Profile/Profile.jsx
@@ -29,7 +29,8 @@ function Profile() {
   }, []);
   const profilePictureUrl = "https://i0.wp.com/vssmn.org/wp-content/uploads/2018/12/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png?w=860&ssl=1"
   return (
-    <div className="profile-container">
+   <div className="profile-page border">
+     <div className="profile-container">
       <div className="profile-sidebar">
         <div className="photo">
           <img src={profilePictureUrl} alt="Profile" />
@@ -39,34 +40,35 @@ function Profile() {
       <div className="profile-content">
         <div className="profile-header">
           <h1>Name : {profile.name} </h1>
-          {/* <p>{profile.name}</p> */}
+          
         </div>
         <div className="profile-text">
           <h2>Username : {profile.username}</h2>
-          {/* <p>{profile.username}</p> */}
+         
         </div>
         <div className="profile-text">
           <h2>Graduation Year : {profile.graduationYear}</h2>
-          {/* <p>{profile.graduationYear}</p> */}
+        
         </div>
         <div className="profile-text">
           <h2>Email : {profile.email}</h2>
-          {/* <p>{profile.email}</p> */}
+          
         </div>
         <div className="profile-text">
           <h2>SID : {profile.SID}</h2>
-          {/* <p>{profile.SID}</p> */}
+          
         </div>
         <div className="profile-text">
           <h2>College : {profile.college}</h2>
-          {/* <p>{profile.college}</p> */}
+          
         </div>
         <div className="profile-text">
           <h2>Branch : {profile.branch}</h2>
-          {/* <p>{profile.branch}</p> */}
+         
         </div>
       </div>
     </div>
+   </div>
   );
 }
 
