aa916ae5cc709e5dd2e89f8bdae15bda5b505ec6
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2024-02-25 03:29:17 +0530

    Update AnswerList.jsx

--- Frontend-Realm/src/components/QuestionPage/AnswerList/AnswerList.jsx
+++ Frontend-Realm/src/components/QuestionPage/AnswerList/AnswerList.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import AnswerItem from "./AnswerItem";
+import './AnswerList.css'
+
 function AnswerList() {
   const [data, setData] = useState({
     title: "",
@@ -10,6 +12,7 @@ function AnswerList() {
     answers: [],
   });
   const [inputAnswer, setInputAnswer] = useState("");
+
   useEffect(() => {
     const fetchData = async () => {
       const res = await axios({
@@ -30,72 +33,84 @@ function AnswerList() {
   }, []);
 
   const submitAnswer = async () => {
-    if(inputAnswer!=""){
+    if (inputAnswer !== "") {
       const res = await axios({
         method: "POST",
         url: "http://localhost:3000/api/question/addanswer",
-        data: {content:inputAnswer, author:localStorage.getItem("username"),quesId:localStorage.getItem("quesId")},
+        data: {
+          content: inputAnswer,
+          author: localStorage.getItem("username"),
+          quesId: localStorage.getItem("quesId"),
+        },
         headers: { Authorization: localStorage.getItem("token") },
       });
-      if(res.status===200){
+      if (res.status === 200) {
         window.location.reload(false);
       }
     }
   };
-  
+
   const likeQues = async () => {
     const res = await axios({
       method: "POST",
       url: "http://localhost:3000/api/question/likeques",
-      data: {quesId:localStorage.getItem("quesId")},
+      data: { quesId: localStorage.getItem("quesId") },
       headers: { Authorization: localStorage.getItem("token") },
     });
-    if(res.status===200){
+    if (res.status === 200) {
       window.location.reload(false);
     }
   };
+
   const dislikeQues = async () => {
     const res = await axios({
       method: "POST",
       url: "http://localhost:3000/api/question/dislikeques",
-      data: {quesId:localStorage.getItem("quesId")},
+      data: { quesId: localStorage.getItem("quesId") },
       headers: { Authorization: localStorage.getItem("token") },
     });
-    if(res.status===200){
+    if (res.status === 200) {
       window.location.reload(false);
     }
   };
 
   return (
-    <div>
-      <div>
-        <div>{data.title}</div>
-        <div>{data.author}</div>
-        <div onClick={likeQues}>{data.likes}</div>
-        <div onClick={dislikeQues}>{data.dislikes}</div>
+    <div className="answer-page">
+    <div className="ans-container">
+      <div className="ans-header">
+        <h2 className="title">Question : {data.title}</h2>
+        <p className="author">Author: {data.author}</p>
+        <div className="like-dislike">
+          <button onClick={likeQues}>Like ({data.likes})</button>
+          <button onClick={dislikeQues}>Dislike ({data.dislikes})</button>
+        </div>
       </div>
-      <div>
+      
+      <div className="answer-list">
         {data.answers.map((answer) => (
+          <div>
           <AnswerItem
+            key={answer.ansdbId} 
             content={answer.content}
             author={answer.author}
             likes={answer.likes}
             dislikes={answer.dislikes}
             ansdbId={answer.ansdbId}
           />
+          </div>
         ))}
       </div>
-      <div>
+      
+      <div className="submit-answer">
         <input
-          onChange={(e) => {
-            setInputAnswer(e.target.value);
-          }}
+          onChange={(e) => setInputAnswer(e.target.value)}
           type="text"
           value={inputAnswer}
-          placeholder="Answer......"
-        />{" "}
-        <div onClick={submitAnswer}>Submit Answer</div>
+          placeholder="Answer..."
+        />
+        <button onClick={submitAnswer}>Submit Answer</button>
       </div>
+    </div>
     </div>
   );
 }
