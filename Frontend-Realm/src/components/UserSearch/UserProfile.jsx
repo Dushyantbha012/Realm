eb6273bd35067354faa4e609138c0a78bf60e023
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2024-02-25 00:32:23 +0530

    Update UserProfile.jsx

--- Frontend-Realm/src/components/UserSearch/UserProfile.jsx
+++ Frontend-Realm/src/components/UserSearch/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue, useRecoilState } from "recoil";
 import axios from "axios";
@@ -11,8 +11,9 @@ import {
   otherSIDState,
   privateRoomIdState,
 } from "../..//atoms/atoms";
+import './UserProfile.css';
 
-function UserProfile({}) {
+function UserProfile() {
   const navigateTo = useNavigate();
   const otherBranch = useRecoilValue(otherBranchState);
   const otherCollege = useRecoilValue(otherCollegeState);
@@ -20,33 +21,32 @@ function UserProfile({}) {
   const otherGraduationYear = useRecoilValue(otherGraduationYearState);
   const otherName = useRecoilValue(otherNameState);
   const otherSID = useRecoilValue(otherSIDState);
-  const [privateRoomId, setPrivateRoomId] =
-      useRecoilState(privateRoomIdState);
+  const [privateRoomId, setPrivateRoomId] = useRecoilState(privateRoomIdState);
 
-    
-    const onClickHandeler=async()=>{
-      const res = await axios({
-        method: "POST",
-        url: "http://localhost:3000/api/chat/createprivate",
-        headers: { authorization: localStorage.getItem("token") },
-        data:{senderMail:localStorage.getItem("email"),recMail:otherEmail}
-      });
-      console.log("room id set is ", res.data.id)
-      setPrivateRoomId(res.data.id)
-      navigateTo("/privatechat")
-    }
+  const onClickHandler = async () => {
+    const res = await axios({
+      method: "POST",
+      url: "http://localhost:3000/api/chat/createprivate",
+      headers: { authorization: localStorage.getItem("token") },
+      data: { senderMail: localStorage.getItem("email"), recMail: otherEmail },
+    });
+    console.log("room id set is ", res.data.id);
+    setPrivateRoomId(res.data.id);
+    navigateTo("/privatechat");
+  };
 
-  
   return (
-    <div>
-      <div>UserProfile</div>
-      <div>Name: {otherName}</div>
-      <div>College : {otherCollege}</div>
-      <div>Branch : {otherBranch}</div>
-      <div>Graduation Year : {otherGraduationYear}</div>
-      <div>SID : {otherSID}</div>
-      <div>E-mail : {otherEmail}</div>
-      <button onClick={onClickHandeler}>Chat</button>
+    <div className="userprofile-component">
+    <div className="user-profile-container">
+      <div className="user-profile-header">User Profile</div>
+      <div className="user-profile-details">Name: <span>{otherName}</span></div>
+      <div className="user-profile-details">College: <span>{otherCollege}</span></div>
+      <div className="user-profile-details">Branch: <span>{otherBranch}</span></div>
+      <div className="user-profile-details">Graduation Year: <span>{otherGraduationYear}</span></div>
+      <div className="user-profile-details">SID: <span>{otherSID}</span></div>
+      <div className="user-profile-details">E-mail: <span>{otherEmail}</span></div>
+      <button className="user-profile-button" onClick={onClickHandler}>Chat</button>
+    </div>
     </div>
   );
 }
